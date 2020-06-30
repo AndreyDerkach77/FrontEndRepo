81f5da04b7727e6223e82b3da0c7d943cde726db
AndreyDerkach77 <andrey.elle@gmail.com>
2020-07-01 01:54:37 +0300

    StudyWork08_React with build

---
+++ StudyWork08_React/build/precache-manifest.adce708b808d7fae2f7f327064a2be13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169cce0a7a0f28c3f6da6f1e80a8c299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d18c6ebcbd0a2e3f6bd",
+    "url": "/static/css/main.f29e0194.chunk.css"
+  },
+  {
+    "revision": "b73c74c793bb1b89a3a8",
+    "url": "/static/js/2.cf93cb8e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cf93cb8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d18c6ebcbd0a2e3f6bd",
+    "url": "/static/js/main.9f8a0765.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
